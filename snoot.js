7ddf2e9362010ec7d7c3d406b9300f5472b0aa2a
Unknown <hstephens333@west-mec.org>
2018-08-06 15:28:06 -0700

    Initial Commit
    Added Javascript to call all the select lists as a number in an alert

--- snoot.js
+++ snoot.js
@@ -7,4 +7,17 @@
 
 */
 
-alert("Yeet");
+"use strict";
+
+// Function to remove select list defaults
+function removeSelectDefaults() {
+    var emptyBoxes = document.getElementsByTagName("select");
+    alert("select lists:" + emptyBoxes.length);
+}
+
+// page load event handlers
+if (window.addEventListener) {
+    window.addEventListener("load", removeSelectDefaults, false);
+} else if (window.attachEvent) {
+        window.attachEvent("onload", removeSelectDefaults);
+}
